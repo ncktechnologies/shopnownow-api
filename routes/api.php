31bfd55e0f6edf3f225b3137ebfa91b795746555
Chuks Ugwuh <chuks@ncktech.com>
2023-11-20 15:14:02 +0100

    updates

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ShoppingListController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
-use League\Flysystem\UrlGeneration\PrefixPublicUrlGenerator;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,89 +26,73 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //Authentification Routes including forgot password and verify otp
-Route::prefix('v1')->group( function(){
+
+
+Route::prefix('v1')->group(function () {
 
     Route::prefix('user')->group(function () {
         Route::prefix('auth')->group(function () {
-            Route::post('/register', 'AuthController@register');
-            Route::post('/login', 'AuthController@login');
-            Route::post('/forgot_password', 'AuthController@forgot_password');
-            Route::post('/resend_otp', 'AuthController@resend_otp');
-            Route::post('/verify_otp', 'AuthController@verify_otp');
-            Route::post('/reset_password', 'AuthController@reset_password');
-            Route::post('/logout', 'AuthController@logout');
+            Route::post('/register', [AuthController::class, 'register']);
+            Route::post('/login', [AuthController::class, 'login']);
+            Route::post('/forgot_password', [AuthController::class, 'forgot_password']);
+            Route::post('/resend_otp', [AuthController::class, 'resend_otp']);
+            Route::post('/verify_otp', [AuthController::class, 'verify_otp']);
+            Route::post('/reset_password', [AuthController::class, 'reset_password']);
+            Route::post('/logout', [AuthController::class, 'logout']);
         });
 
+        Route::middleware(['auth:user'])->group(function () {
+            Route::prefix('profile')->group(function () {
+                Route::get('show', [UserController::class, 'profile']);
+                Route::post('change_password', [UserController::class, 'change_password']);
+            });
 
-        Route::middleware(['auth:user'])->group(
-            function () {
-                Route::prefix('profile')->group(function () {
-                    Route::get('show', 'UserController@profile');
-                    Route::post('change_password', 'UserController@change_password');
-                });
-
-                Route::prefix('orders')->group(function () {
-                    Route::post('/orders', 'OrderController@store'); // Create an order
-                    Route::get('/orders/{order}', 'OrderController@show'); // Get order details
-                    Route::put('/orders/{order}', 'OrderController@update'); // Update order
-                    Route::delete('/orders/{order}', 'OrderController@destroy'); // Delete order
-                });
-            }
-        );
+            Route::prefix('orders')->group(function () {
+                Route::post('/orders', [OrderController::class, 'store']); // Create an order
+                Route::get('/orders/{order}', [OrderController::class, 'show']); // Get order details
+                Route::put('/orders/{order}', [OrderController::class, 'update']); // Update order
+                Route::delete('/orders/{order}', [OrderController::class, 'destroy']); // Delete order
+            });
+        });
 
         Route::prefix('shopping_list')->group(function () {
-            // Shopping List Routes
-            Route::post('/create_shopping_list', 'ShoppingListController@create');
-            Route::post('/place_order/{list_id}', 'OrderController@place');
+            Route::post('/create_shopping_list', [ShoppingListController::class, 'create']);
+            Route::post('/place_order/{list_id}', [OrderController::class, 'place']);
         });
-
 
         Route::prefix('payment')->group(function () {
-            // Payment Routes
-            Route::post('/process', 'PaymentController@process'); // Process a payment
-            Route::get('/payment/{payment}', 'PaymentController@show'); // Get payment details
-            Route::put('/payment/{payment}', 'PaymentController@update'); // Update payment
-            Route::delete('/payments/{payment}', 'PaymentController@destroy'); // Delete payment
+            Route::post('/process', [PaymentController::class, 'process']);
+            Route::get('/payment/{payment}', [PaymentController::class, 'show']);
+            Route::put('/payment/{payment}', [PaymentController::class, 'update']);
+            Route::delete('/payments/{payment}', [PaymentController::class, 'destroy']);
         });
     });
-
-
 
     Route::prefix('admin')->group(function () {
-
         Route::prefix('auth')->group(function () {
-            Route::post('/signup', 'AdminController@signup');
-            Route::post('/login', 'AdminController@login');
-            Route::post('/logout', 'AdminController@logout');
-            Route::post('/forgot_password', 'AdminController@forgot_password');
+            Route::post('/signup', [AdminController::class, 'signup']);
+            Route::post('/login', [AdminController::class, 'login']);
+            Route::post('/logout', [AdminController::class, 'logout']);
+            Route::post('/forgot_password', [AdminController::class, 'forgot_password']);
         });
     });
 
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
+    Route::post('/payment', [PaymentController::class, 'process']);
 
-
-
-    // User Routes
-    Route::post('/users', 'UserController@store'); // Create a user
-    Route::get('/users/{user}', 'UserController@show'); // Get user details
-    Route::put('/users/{user}', 'UserController@update'); // Update user
-    Route::delete('/users/{user}', 'UserController@destroy'); // Delete user
-
-
-
-    // Payment Routes
-    Route::post('/payment', 'PaymentController@process'); // Process a payment
-
-    // Admin Routes
     Route::group(['prefix' => 'admin'], function () {
-        Route::get('/users', 'AdminController@users'); // Get all users
-        Route::get('/orders', 'AdminController@orders'); // Get all orders
+        Route::get('/users', [AdminController::class, 'users']);
+        Route::get('/orders', [AdminController::class, 'orders']);
         // Add more admin routes as needed
     });
 
-    // Authentication Routes
-    Route::post('/register', 'AuthController@register'); // Register a new user
-    Route::post('/login', 'AuthController@login'); // Login a user
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
+
 
 
