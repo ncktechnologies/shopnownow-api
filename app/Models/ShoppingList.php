6031aeb8282864d7731a8821c5b364412639349a
Chuks Ugwuh <chuks@ncktech.com>
2023-11-21 21:59:37 +0100

    test

--- app/Models/ShoppingList.php
+++ app/Models/ShoppingList.php
@@ -10,7 +10,7 @@ class ShoppingList extends Model
     use HasFactory;
 
     // Add fillable products and quantities to the model
-    protected $fillable = ['product_ids', 'quantities'];
+    protected $fillable = ['product_ids', 'quantities', 'user_id'];
 
     // Cast the product_ids and quantities to arrays
     protected $casts = [
