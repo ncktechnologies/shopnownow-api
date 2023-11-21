82e3809cd0741fbfd9cce616b08dd3d8af423f73
Chuks Ugwuh <chuks@ncktech.com>
2023-11-21 21:54:39 +0100

    added error handling

--- app/Http/Controllers/ShoppingListController.php
+++ app/Http/Controllers/ShoppingListController.php
@@ -10,32 +10,36 @@ class ShoppingListController extends Controller
 
     public function createList(Request $request)
     {
-        // Get the authenticated user using Laravel's authentication
-        $user = auth()->user();
+        try {
+            // Get the authenticated user using Laravel's authentication
+            $user = auth()->user();
 
-        // Validate the incoming request data
-        $validatedData = $request->validate([
-            'products.*.id' => 'required|integer',
-            'products.*.quantity' => 'required|integer',
-        ]);
+            // Validate the incoming request data
+            $validatedData = $request->validate([
+                'products.*.id' => 'required|integer',
+                'products.*.quantity' => 'required|integer',
+            ]);
 
-        // Extract product IDs and quantities from the products array
-        $products = collect($validatedData['products']);
-        $productIds = $products->pluck('id');
-        $quantities = $products->pluck('quantity');
+            // Extract product IDs and quantities from the products array
+            $products = collect($validatedData['products']);
+            $productIds = $products->pluck('id');
+            $quantities = $products->pluck('quantity');
 
-        // Add product IDs and quantities to the validated data
-        $validatedData['product_ids'] = json_encode($productIds);
-        $validatedData['quantities'] = json_encode($quantities);
+            // Add product IDs and quantities to the validated data
+            $validatedData['product_ids'] = json_encode($productIds);
+            $validatedData['quantities'] = json_encode($quantities);
 
-        // Associate the user ID with the shopping list
-        $validatedData['user_id'] = $user->id;
+            // Associate the user ID with the shopping list
+            $validatedData['user_id'] = $user->id;
 
-        // Create a new shopping list
-        $shoppingList = ShoppingList::create($validatedData);
+            // Create a new shopping list
+            $shoppingList = ShoppingList::create($validatedData);
 
-        // You may choose to send a response with the newly created shopping list's data
-        return response()->json(['message' => 'Shopping list created successfully', 'shopping list' => $shoppingList], 201);
+            // You may choose to send a response with the newly created shopping list's data
+            return response()->json(['message' => 'Shopping list created successfully', 'shopping list' => $shoppingList], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred while creating the shopping list', 'error' => $e->getMessage()], 500);
+        }
     }
 
 }
