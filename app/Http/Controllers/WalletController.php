f28a165406d0273ca79bea94c21c21a28fd78d09
Chuks Ugwuh <chuks@ncktech.com>
2023-11-21 22:45:06 +0100

    added some exception handling

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -6,99 +6,125 @@ use Illuminate\Http\Request;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 
+
 class WalletController extends Controller
 {
     public function balance()
     {
-        // Get the authenticated user
-        $user = Auth::user();
+        try {
+            // Get the authenticated user
+            $user = Auth::user();
 
-        // Return the user's balance and loyalty points
-        return response()->json(['wallet_balance' => $user->wallet, 'loyalty_points' => $user->loyalty_points]);
+            // Return the user's balance and loyalty points
+            return response()->json(['wallet_balance' => $user->wallet, 'loyalty_points' => $user->loyalty_points]);
+        } catch (\Exception $e) {
+            // Handle any unexpected errors
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 
     public function fundWallet(Request $request)
     {
-        // Validate the request
-        $request->validate([
-            'amount' => 'required|numeric|min:0.01',
-        ]);
+        try {
+            // Validate the request
+            $request->validate([
+                'amount' => 'required|numeric|min:0.01',
+            ]);
 
-        // Get the authenticated user
-        $user = Auth::user();
+            // Get the authenticated user
+            $user = Auth::user();
 
-        // Update the user's balance
-        $user->wallet += $request->amount;
-        $user->save();
+            // Update the user's balance
+            $user->wallet += $request->amount;
+            $user->save();
 
-        // Create a new transaction
-        $transaction = new Transaction([
-            'user_id' => $user->id,
-            'amount' => $request->amount,
-            'type' => 'credit',
-            'reference' => $request->reference,
-            'status' => 'success',
-            'message' => 'Wallet funding',
-        ]);
-        $transaction->save();
+            // Create a new transaction
+            $transaction = new Transaction([
+                'user_id' => $user->id,
+                'amount' => $request->amount,
+                'type' => 'credit',
+                'reference' => $request->reference,
+                'status' => 'success',
+                'message' => 'Wallet funding',
+            ]);
+            $transaction->save();
 
-        return response()->json(['message' => 'Wallet funded successfully']);
+            return response()->json(['message' => 'Wallet funded successfully']);
+        } catch (\Exception $e) {
+            // Handle validation errors or unexpected errors
+            return response()->json(['error' => $e->getMessage()], $e instanceof \Illuminate\Validation\ValidationException ? 422 : 500);
+        }
     }
 
     public function withdrawFunds(Request $request)
     {
-        // Validate the request
-        $request->validate([
-            'amount' => 'required|numeric|min:0.01',
-        ]);
+        try {
+            // Validate the request
+            $request->validate([
+                'amount' => 'required|numeric|min:0.01',
+            ]);
 
-        // Get the authenticated user
-        $user = Auth::user();
+            // Get the authenticated user
+            $user = Auth::user();
 
-        // Check if the user has enough balance
-        if ($user->wallet < $request->amount) {
-            return response()->json(['message' => 'Insufficient balance'], 400);
+            // Check if the user has enough balance
+            if ($user->wallet < $request->amount) {
+                return response()->json(['message' => 'Insufficient balance'], 400);
+            }
+
+            // Update the user's balance
+            $user->wallet -= $request->amount;
+            $user->save();
+
+            // Create a new transaction
+            $transaction = new Transaction([
+                'user_id' => $user->id,
+                'amount' => $request->amount,
+                'reference' => $request->reference,
+                'status' => 'success',
+                'type' => 'debit',
+                'message' => 'Wallet withdrawal',
+            ]);
+            $transaction->save();
+
+            return response()->json(['message' => 'Withdrawal successful']);
+        } catch (\Exception $e) {
+            // Handle validation errors or unexpected errors
+            return response()->json(['error' => $e->getMessage()], $e instanceof \Illuminate\Validation\ValidationException ? 422 : 500);
         }
-
-        // Update the user's balance
-        $user->wallet -= $request->amount;
-        $user->save();
-
-        // Create a new transaction
-        $transaction = new Transaction([
-            'user_id' => $user->id,
-            'amount' => $request->amount,
-            'reference' => $request->reference,
-            'status' => 'success',
-            'type' => 'debit',
-            'message' => 'Wallet withdrawal',
-        ]);
-        $transaction->save();
-
-        return response()->json(['message' => 'Withdrawal successful']);
     }
 
     public function transactionHistory()
     {
-        // Get the authenticated user
-        $user = Auth::user();
+        try {
+            // Get the authenticated user
+            $user = Auth::user();
 
-        // Get the user's transactions
-        $transactions = Transaction::where('user_id', $user->id)->get();
+            // Get the user's transactions
+            $transactions = Transaction::where('user_id', $user->id)->get();
 
-        return response()->json(['transactions' => $transactions]);
+            return response()->json(['transactions' => $transactions]);
+        } catch (\Exception $e) {
+            // Handle any unexpected errors
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
 
     public function transactionDetails($id)
     {
-        // Get the transaction
-        $transaction = Transaction::findOrFail($id);
+        try {
+            // Get the transaction
+            $transaction = Transaction::findOrFail($id);
 
-        // Check if the transaction belongs to the authenticated user
-        if ($transaction->user_id != Auth::id()) {
-            return response()->json(['message' => 'Unauthorized'], 403);
+            // Check if the transaction belongs to the authenticated user
+            if ($transaction->user_id != Auth::id()) {
+                return response()->json(['message' => 'Unauthorized'], 403);
+            }
+
+            return response()->json(['transaction' => $transaction]);
+        } catch (\Exception $e) {
+            // Handle not found error or unexpected errors
+            return response()->json(['error' => $e->getMessage()], $e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException ? 404 : 500);
         }
-
-        return response()->json(['transaction' => $transaction]);
     }
 }
