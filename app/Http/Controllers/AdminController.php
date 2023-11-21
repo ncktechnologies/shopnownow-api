9cecc98fa0983d2bd00c72dea06e681e329466a4
Chuks Ugwuh <chuks@ncktech.com>
2023-11-21 02:12:38 +0100

    added auth feature

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Hash;
+use Exception;
 
 class AdminController extends Controller
 {
