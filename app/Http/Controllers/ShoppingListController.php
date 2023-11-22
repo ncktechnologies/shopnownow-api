df52e40584c4a26baf1c5ffb42e88cf6b2a1a926
Chuks Ugwuh <chuks@ncktech.com>
2023-11-22 17:57:05 +0100

    update

--- app/Http/Controllers/ShoppingListController.php
+++ app/Http/Controllers/ShoppingListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ShoppingList;
 use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ShoppingListController extends Controller
