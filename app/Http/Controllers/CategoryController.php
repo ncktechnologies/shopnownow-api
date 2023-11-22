2127f046d6f1094bd942fb3a300c7c1f4e081125
Chuks Ugwuh <chuks@ncktech.com>
2023-11-22 09:22:34 +0100

    updates

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Exception;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
-        return response()->json($categories);
+        try {
+            $categories = Category::all();
+            return response()->json($categories);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'An error occurred while retrieving the categories', 'error' => $e->getMessage()], 500);
+        }
     }
 
     public function create(Request $request)
     {
+        try{
         $request->validate([
             'name' => 'required',
             'tax' => 'required|numeric',
@@ -33,45 +39,61 @@ class CategoryController extends Controller
             $data['thumbnail_url'] = asset('storage/' . $thumbnailPath);
         }
 
-        $category = Category::create($data);
+            $category = Category::create($data);
 
-        return response()->json($category, 201);
+            return response()->json($category, 201);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'An error occurred while creating the category', 'error' => $e->getMessage()], 500);
+        }
     }
 
     public function update(Request $request, Category $category)
     {
-        $request->validate([
-            'name' => 'required',
-            'tax' => 'required|numeric',
-            'delivery_option' => 'required|boolean',
-            'discount_option' => 'required|boolean',
-            'discount_type' => 'nullable|in:percentage,fixed',
-            'discount_value' => 'nullable|numeric',
-            'thumbnail' => 'nullable|image',
-            'band_id' => 'required|exists:bands,id', // Add this line
-        ]);
+        try {
+            $request->validate([
+                'name' => 'required',
+                'tax' => 'required|numeric',
+                'delivery_option' => 'required|boolean',
+                'discount_option' => 'required|boolean',
+                'discount_type' => 'nullable|in:percentage,fixed',
+                'discount_value' => 'nullable|numeric',
+                'thumbnail' => 'nullable|image',
+                'band_id' => 'required|exists:bands,id', // Add this line
+            ]);
 
-        $data = $request->all();
+            $data = $request->all();
 
-        if ($request->hasFile('thumbnail')) {
-            $path = $request->file('thumbnail')->store('thumbnails', 'public');
-            $data['thumbnail'] = asset($path);
+            if ($request->hasFile('thumbnail')) {
+                $path = $request->file('thumbnail')->store('thumbnails', 'public');
+                $data['thumbnail'] = asset($path);
+            }
+
+            $category->update($data);
+
+            return response()->json($category);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'An error occurred while updating the category', 'error' => $e->getMessage()], 500);
         }
-
-        $category->update($data);
-
-        return response()->json($category);
     }
 
     public function show(Category $category)
     {
-        return response()->json($category);
+        try {
+            return response()->json($category);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'An error occurred while retrieving the category', 'error' => $e->getMessage()], 500);
+        }
     }
+
 
     public function hide(Category $category)
     {
-        $category->update(['hidden' => !$category->hidden]);
+        try {
+            $category->update(['hidden' => !$category->hidden]);
 
-        return response()->json($category);
+            return response()->json($category);
+        } catch (Exception $e) {
+            return response()->json(['message' => 'An error occurred while updating the category visibility', 'error' => $e->getMessage()], 500);
+        }
     }
 }
