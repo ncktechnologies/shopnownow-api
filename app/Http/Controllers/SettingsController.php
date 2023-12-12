718a47923bd53381b76787ae18bfd9c3ad6bfc76
Chuks Ugwuh <chuks@ncktech.com>
2023-12-12 16:17:15 +0100

    rollback

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -41,7 +41,7 @@ class SettingsController extends Controller
         $settingsArray = [];
 
         foreach ($settings as $setting) {
-            $settingsArray[] = $setting->key;
+            $settingsArray[] = ['key' => $setting->key, 'value' => $setting->value];
         }
 
         return response()->json(['settings' => $settingsArray]);
