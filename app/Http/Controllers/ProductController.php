aecff8b455ae310f2c4130cfca3dd372d366d7a2
Chuks Ugwuh <chuks@ncktech.com>
2024-01-02 09:57:23 +0100

    Update pagination limit in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::orderBy('created_at', 'desc')->paginate(50);
+        $products = Product::orderBy('created_at', 'desc')->paginate(25);
         return response()->json($products);
     }
     /**
