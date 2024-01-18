d0fda13ef592c66e9dd8885b5bd32993e3ad20d4
Chuks Ugwuh <chuks@ncktech.com>
2024-01-18 19:29:26 +0100

    Add route for terms and conditions page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/terms-and-conditions', function () {
+    return view('terms-and-conditions');
+});
