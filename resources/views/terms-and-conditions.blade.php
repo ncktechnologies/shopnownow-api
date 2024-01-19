cb3b9d48b3109988291a9d2b6c2640328a149578
Chuks Ugwuh <chuks@ncktech.com>
2024-01-19 19:48:56 +0100

    Update terms and conditions page

--- resources/views/terms-and-conditions.blade.php
+++ resources/views/terms-and-conditions.blade.php
@@ -6,7 +6,72 @@
     <title>Terms and Conditions</title>
 </head>
 <body>
-    <h1>Terms and Conditions</h1>
-    <!-- Write your terms and conditions here -->
+    <h1>Terms & Conditions</h1>
+    <p>In these Terms and Conditions, the "Company" or "We" will mean ShopNowNow and the "Customer" will mean any person, firm, company, or body which places an order with the Company. Please read these Terms and Conditions carefully before using the application. By using the application or any part of it, you agree that you have read these terms and conditions and that you accept and agree to be bound by them.</p>
+
+    <p>These Terms and Conditions cover the use of ShopNowNow website, ShopNowNow App, and Phone calls for ordering and customer care services.</p>
+
+    <h2>General</h2>
+    <ul>
+        <li>The Company shall make every effort to ensure that prices, details, and sizes of products on this website are up to date. Prices are subject to change without prior notice and all orders are subject to Company's acceptance at its sole discretion and stock availability.</li>
+        <li>The Company shall make every effort to ensure products displayed on the website are in stock. If from time to time products become out of stock, the Company reserves the right to offer alternative product/s to the Customer of similar value and style.</li>
+        <li>The prices charged are those applicable on the day you order.</li>
+        <li>All orders are subject to confirmation of final availability, and ShopNowNow reserves the right to reject the order in the event that any of the products or services requested is unavailable. In the event that we are unable to fulfill any of the products or services in your order, we will notify you by phone or email before delivery.</li>
+    </ul>
+
+    <h2>Registration</h2>
+    <ul>
+        <li>You are required to register as a User before placing any order through this application to enjoy the full benefits available to you. During the registration:</li>
+        <ol>
+            <li>You must provide us with accurate, complete and up-to-date registration information (Name, E-mail, and Phone number);</li>
+            <li>You must safeguard your username and password;</li>
+            <li>You authorize us to assume that any person using the application with your username and password is either you or is authorized to act for you;</li>
+        </ol>
+        <li>The Company has the absolute discretion to refuse registration of a potential customer and to terminate the registration of any customer for any reason whatsoever.</li>
+        <li>You should inform us immediately of any changes to the information that you provided so that we can communicate with you effectively.</li>
+    </ul>
+
+    <h2>Damage</h2>
+    <p>The Customer is required to examine the goods delivered immediately upon delivery for any omission and/or damages. Claims (if any) must be lodged to our customer service feedback line (07025238689) at the time of delivery, otherwise, we shall have the right to refuse your claim after delivery.</p>
+    <h2>Refund/Replace Policy</h2>
+    <ul>
+        <li>It is the receiver's responsibility to check the condition of goods received, at the time of delivery.</li>
+        <li>Refund/Replacement of goods purchased is only available if the goods are defective subject to other conditions listed below.</li>
+        <li>Our average delivery time is about 90 minutes; however, certain situations outside our control (like weather condition and rush hour periods) may cause an extension of our delivery time. Customers shall, however, reserve the rights to decline delivery of goods ordered after the expiration of 120 minutes from the time the order was placed. The full amount shall be credited into the bank account provided by the customer.</li>
+        <li>Refund of amounts less than N5,000 will be credited into Customer's ShopNowNow online wallet (for ShopNowNow website and App orders only) which can be used on the next online purchase, while refund requests above N5,000 will be credited into customer’s bank account (less transaction charges) within 24 - 48 hours upon confirmation. Customers without a ShopNowNow wallet account shall have their refunds paid into their bank accounts less bank charges. Please note that bank charges will not be deducted for refunds due to late delivery or defective meal products.</li>
+        <li>In the event that you would like to claim a refund or return for a defective product, please send a mail or call our customer service hotline within 60 minutes from the time of delivery.</li>
+    </ul>
+
+    <h2>Payment Terms</h2>
+    <ul>
+        <li>We accept payments via your Debit/Credit Card, ShopNowNow Wallet, and Bank Transfers.</li>
+        <li>All product prices listed are in Naira.</li>
+        <li>To provide secure online commerce transactions, we employ Internet's Secure Sockets Layer (SSL) technology. It encrypts your personal information, including credit card number, name, and address, so that it would not be read as the information travels over the Internet.</li>
+        <li>There is no limit of purchase on this application if you pay by ShopNowNow Wallet, Bank Transfers, or Debit/Credit Card.</li>
+        <li>Your offer to purchase made on ShopNowNow shall be subject to our sole discretion and may not be accepted subject to the availability of stock. A final and conclusive invoice will be presented to you upon delivery.</li>
+        <li>ShopNowNow may from time to time set credit limits for individual customers and ShopNowNow reserves the right to limit sales including the right to decline sales to any customers.</li>
+        <li>ShopNowNow reserves the right to change the payment terms for any order without prior notice.</li>
+    </ul>
+
+    <h2>Delivery</h2>
+    <ul>
+        <li>We offer charged delivery (single location only) within the Island (Victoria Island, Ikoyi, Oniru, Lekki-Ajah, and Marina) only for now. We may add or remove other locations from time to time.</li>
+        <li>All orders are usually delivered within 120 minutes. However, delivery location and certain situations outside our control (e.g. rush hour, weather, etc.) may affect the time your order is delivered.</li>
+        <ul>
+            <li>Due to restricted access to some certain offices, customers there must collect deliveries at designated location(s).</li>
+            <li>Delivery service is not available to areas that cannot be reached directly by our Motorbikes/vehicles, or buildings without a functioning lift to the 5th floor and above.</li>
+            <li>Delivery service will be suspended during bad weather. In this event customers will be contacted to re-schedule delivery time.</li>
+            <li>We reserve the right to charge an additional delivery fee if the recipient is not available to accept delivery at the nominated delivery time.</li>
+            <li>It is the responsibility of the customer to notify ShopNowNow immediately of any change to their delivery address.</li>
+            <li>We reserve the right to decline delivery to any customer at our sole discretion absolutely.</li>
+        </ul>
+    </ul>
+
+    <h2>Force Majeure</h2>
+    <p>The Company shall not be liable for any delay or failure in its performance caused by or resulting from acts of God, fire, flood, accident, riot, war, government intervention, embargoes, strikes, labor difficulties, equipment failures, or any other causes beyond the control of the Company.</p>
+
+</body>
+</html>
+
 </body>
 </html>
